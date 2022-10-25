a4b6b91fa6b7502d914490eb7816cabbd3d34b1c
Hear No Darkness <hearnodarkness@gmail.com>
2022-10-24 22:00:14 -0300

    Basic Controller Movement

---
+++ ArchitectureExplorer/Source/ArchitectureExplorer/VRCharacter.cpp
@@ -0,0 +1,57 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "VRCharacter.h"
+
+#include "Camera/CameraComponent.h"
+
+
+// Sets default values
+AVRCharacter::AVRCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	Camera->SetupAttachment(GetRootComponent());
+	
+}
+
+// Called when the game starts or when spawned
+void AVRCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AVRCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	
+}
+
+// Called to bind functionality to input
+void AVRCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	// Respond every frame to the values of our two movement axes, "MoveX" and "MoveY".
+	PlayerInputComponent->BindAxis(TEXT("Forward"), this, &AVRCharacter::MoveForward);
+	PlayerInputComponent->BindAxis(TEXT("Right"), this, &AVRCharacter::MoveRight);
+
+}
+
+
+void AVRCharacter::MoveForward(float throttle)
+{
+	// Move at speed forward or backward
+	AddMovementInput(throttle * Camera->GetForwardVector() * speed);
+} 
+
+void AVRCharacter::MoveRight(float throttle)
+{
+	// Move at speed right or left
+	AddMovementInput(throttle * Camera->GetRightVector() * speed);
+}
