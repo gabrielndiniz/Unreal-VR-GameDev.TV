98b59d0542ae52e40d6d4332d7935f141e3d18ff
Hear No Darkness <hearnodarkness@gmail.com>
2022-11-24 22:44:32 -0300

    Changing to Unreal 4.27
    It get worse

---
+++ U4/ArchitectureExplorer/Source/ArchitectureExplorer/VRCharacter.h
@@ -0,0 +1,74 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "VRCharacter.generated.h"
+
+UCLASS()
+class ARCHITECTUREEXPLORER_API AVRCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AVRCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+private:
+	
+	bool FindTeleportDestination(FVector &OutLocation);
+	void UpdateDestinationMarker();
+
+	void MoveForward(float throttle);
+	void MoveRight(float throttle);
+
+	void BeginTeleport();
+	void FinishTeleport();
+
+	void StartFade(float FromAlpha, float ToAlpha);
+
+private:
+	
+	UPROPERTY()
+	class UCameraComponent* Camera;
+	UPROPERTY()
+	class USceneComponent* VRRoot;
+
+	UPROPERTY(VisibleAnywhere)
+	class UStaticMeshComponent* DestinationMarker;
+	
+	UPROPERTY()
+	class UPostProcessComponent* PostProcessComponent;
+
+	UPROPERTY()
+	class UMaterialInstanceDynamic* BlinkerMaterialInstance;
+
+private:
+	
+	UPROPERTY(EditAnywhere)
+	float MaxTeleportDistance = 1000;
+
+	UPROPERTY(EditAnywhere)
+	float TeleportFadeTime = 1;
+
+	UPROPERTY(EditAnywhere)
+	FVector TeleportProjectionExtent = FVector(100, 100, 100);
+
+	
+	UPROPERTY(EditAnywhere)
+	class UMaterialInterface * BlinkerMaterialBase;
+
+	
+};
